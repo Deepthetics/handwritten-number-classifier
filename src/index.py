6701d8e6c4915134d12580ca198e20ded2f15855
Vili Sirkeoja <vili.sirkeoja@helsinki.fi>
2022-11-18 01:01:25 +0200

    Modify index.py

--- src/index.py
+++ src/index.py
@@ -6,13 +6,24 @@ from image_data_processing import extract, to_binary
 
 #print(os.getcwd())
 
+# Read MNIST data into 2D-arrays
 train_data = pd.read_csv('data/mnist_train.csv')
 test_data = pd.read_csv('data/mnist_test.csv')
 
+# Extract image and label data
 train_X_grey, train_y = extract(train_data)
 test_X_grey, test_y = extract(test_data)
 
-train_X = to_binary(train_X_grey)
-test_X = to_binary(test_X_grey)
-train_X.to_csv('train_x.csv')
-test_X.to_csv('test_x.csv')
+# Convert greyscale image data to binary (black and white) image data
+#train_X = to_binary(train_X_grey)
+#test_X = to_binary(test_X_grey)
+
+# Store converted data in ./data as 'train_x.csv' and 'test_x.csv'
+#train_X.to_csv('train_x.csv')
+#test_X.to_csv('test_x.csv')
+
+# Read binary (black and white) image data into 2D-arrays
+train_X = pd.read_csv('data/train_x.csv')
+test_X = pd.read_csv('data/test_x.csv')
+
+#...
