80269dc93c55d8beb10a5bbfaf6e0a7347a33a53
Vili Sirkeoja <vili.sirkeoja@helsinki.fi>
2022-12-12 03:23:52 +0200

    Fix errors in distance functions, add tests for functions in distance_measures.py

--- src/distance_measures.py
+++ src/distance_measures.py
@@ -6,7 +6,7 @@ COORDINATES_BY_DISTANCE = generate_coordinates_by_distance()
 
 def distance_d6(image1, image2):
     point_count = 0
-    aux_sum = 0
+    result = 0
 
     image1 = to_array(image1)
     image2 = to_array(image2)
@@ -20,14 +20,14 @@ def distance_d6(image1, image2):
                 for x, y, distance in COORDINATES_BY_DISTANCE[(i, j)]:
 
                     if image2[x, y] == 0:
-                        aux_sum += distance
+                        result += distance
                         break
 
-    return 1/point_count*aux_sum
+    return 1/point_count*result
 
 def distance_d6_mod(image1, image2):
     point_count = 0
-    aux_sum = 0
+    result = 0
 
     image1 = to_array(image1)
     image2 = to_array(image2)
@@ -41,10 +41,10 @@ def distance_d6_mod(image1, image2):
                 for x, y, distance in COORDINATES_BY_DISTANCE[(i, j)]:
 
                     if image2[x, y] == 0:
-                        aux_sum += distance
+                        result += distance
                         break
 
-    return aux_sum
+    return result
 
 def distance_f2(distance1, distance2):
     return max(distance1, distance2)
@@ -59,10 +59,10 @@ def distance_d22(image1, image2):
 
 def distance_d23(image1, image2):
     distance1 = distance_d6(image1, image2)
-    distance2 = distance_d6(image1, image2)
+    distance2 = distance_d6(image2, image1)
     return distance_f3(distance1, distance2)
 
 def distance_d23_mod(image1, image2):
     distance1 = distance_d6_mod(image1, image2)
-    distance2 = distance_d6_mod(image1, image2)
+    distance2 = distance_d6_mod(image2, image1)
     return distance_f3(distance1, distance2)
--- src/tests/distance_measures_test.py
+++ src/tests/distance_measures_test.py
@@ -1,5 +1,39 @@
 import unittest
+from math import sqrt
+import numpy as np
+import pandas as pd
+from distance_measures import distance_d6, distance_d6_mod, distance_f2, distance_f3
 
 
 class TestDistanceMeasures(unittest.TestCase):
-    pass
+    def setUp(self):
+        self.image1 = pd.Series([0]*784)
+        self.image2 = np.array([0]+[1]*27)
+        for i in range(27):
+            self.image2 = np.concatenate((self.image2, np.array([1]*28)))
+        self.image2 = pd.Series(self.image2)
+        self.image3 = np.array([1]*28)
+        for i in range(26):
+            self.image3 = np.concatenate((self.image3, [1]*28))
+        self.image3 = np.concatenate((self.image3, [1]*27+[0]))
+        self.image3 = pd.Series(self.image3)
+
+    def test_distance_d6_calculates_distance_correctyle(self):
+        self.assertAlmostEqual(distance_d6(self.image1, self.image1), 0)
+        self.assertAlmostEqual(distance_d6(self.image2, self.image2), 0)
+        self.assertAlmostEqual(distance_d6(self.image2, self.image3), 1/1*27*sqrt(2))
+
+    def test_distance_d6_mod_calculates_distance_correctyle(self):
+        self.assertAlmostEqual(distance_d6_mod(self.image1, self.image1), 0)
+        self.assertAlmostEqual(distance_d6_mod(self.image2, self.image2), 0)
+        self.assertAlmostEqual(distance_d6_mod(self.image2, self.image3), 1/1*27*sqrt(2))
+
+    def test_distance_f2_calculates_distance_correctyle(self):
+        self.assertEqual(distance_f2(1, 1), 1)
+        self.assertEqual(distance_f2(2, 2.1), 2.1)
+        self.assertEqual(distance_f2(10, 5.2), 10)
+
+    def test_distance_f3_calculates_distance_correctyle(self):
+        self.assertEqual(distance_f3(1, 1), (1+1)/2)
+        self.assertEqual(distance_f3(2, 2.1), (2+2.1)/2)
+        self.assertEqual(distance_f3(10, 5.2), (10+5.2)/2)
